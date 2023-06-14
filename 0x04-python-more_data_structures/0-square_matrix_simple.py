ac33a0b3f54c95a6ecc35c56073644a7f0161b12
Ayowale <haydexwisdom@gmail.com>
2023-06-14 20:06:30 +0000

    Task 0 fixed

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     if isinstance(matrix, list):
-        squared = list(map(lambda x: list(map(lambda y: y * y, x)) ,matrix))
+        squared = list(map(lambda x: list(map(lambda y: y * y, x)), matrix))
         return squared
