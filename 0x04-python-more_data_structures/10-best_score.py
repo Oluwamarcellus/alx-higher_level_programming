30fc6dfe51d4958a435ab4bd83979f012bfa227a
Ayowale <haydexwisdom@gmail.com>
2023-06-14 21:45:37 +0000

    Task 10 fixed

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if isinstance(a_dictionary, dict):
-        ele = ""
-        highest = a_dictionary[list(a_dictionary.keys())[0]]
-        for x in a_dictionary:
-            if a_dictionary[x] > highest:
-                ele = x
-        return ele
+    if a_dictionary:
+        return (max(a_dictionary, key=a_dictionary.get))
     else:
         return None
