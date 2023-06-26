bdf5aa55e3db14feb2d34c58e5c1cd2ba187e2b9
Ayowale <haydexwisdom@gmail.com>
2023-06-26 19:26:03 +0000

    task 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     try:
-        if my_list:
-            for i in range(x):
-                if isinstance(my_list[i], int):
-                    print("{:d}".format(my_list[i]), end="")
-            print()
+        sum = 0
+        for i in range(x):
+            if isinstance(my_list[i], int):
+                sum += 1
+                print("{:d}".format(my_list[i]), end="")
+        print()
+        return sum
     except IndexError:
-        pass
+        raise
