d224828c06c910e120085d702d8152bad10fadc8
Ayowale <haydexwisdom@gmail.com>
2023-06-26 18:21:00 +0000

    Task 1

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        if isinstance(value, int):
+            print(value)
+            return True
+        raise ValueError
+    except ValueError:
+        return False
