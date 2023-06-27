85e0c36d3498c81f58a1fed50137c938b9374272
Ayowale <haydexwisdom@gmail.com>
2023-06-27 21:13:07 +0000

    Task 2 fixed

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,11 +6,8 @@ class Square:
     """Square class"""
     def __init__(self, size=0):
         """initialization"""
-        try:
-            if type(size) != int:
-                raise TypeError("size must be an integer")
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = size
-        except Exception:
-            raise
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
