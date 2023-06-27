ff68f135c56620a983195cab75b802d5aa9c2c4d
Ayowale <haydexwisdom@gmail.com>
2023-06-27 21:09:30 +0000

    Task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""class Square that defines a square"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size=0):
+        """initialization"""
+        try:
+            if type(size) != int:
+                raise TypeError("size must be an integer")
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = size
+        except Exception:
+            raise
