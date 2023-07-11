480228ee366bce85f0925d833ffe23a7d81d3f7e
Ayowale <haydexwisdom@gmail.com>
2023-07-11 20:48:45 +0000

    Task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+10-student module
+
+"""
+
+
+class Student:
+    """
+    class Student that defines a student by:
+    ---- first_name
+    ---- last_name
+    ---- age
+
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        initializing the class instance/ object
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        retrieves a dictionary representation of a Student isinstance
+        """
+
+        a = attrs
+        if a and type(a) is list and a[0] and type(a[0]) is str:
+            return {x: self.__dict__[x] for x in self.__dict__ if x in a}
+        return self.__dict__
