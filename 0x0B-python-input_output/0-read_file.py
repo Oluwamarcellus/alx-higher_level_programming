42eccea8cd840e52687d85d062208995c690162e
Ayowale <haydexwisdom@gmail.com>
2023-07-11 18:17:02 +0000

    Task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+0-read_file module
+
+
+"""
+
+
+def read_file(filename=""):
+    """
+    a function that reads a text file (UTF8) and prints it to stdout
+    """
+
+    with open(filename, encoding="utf-8") as f:
+        print(f.read().strip())
