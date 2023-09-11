c1914dae9daf9099b614767b2178457c686b3f81
Ayowale <haydexwisdom@gmail.com>
2023-09-11 17:21:53 +0100

    Task 3 and 11 fixed

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,17 +1,8 @@
 #!/usr/bin/node
-let argv = process.argv;
-let secBig = 0;
-let biggest = 0;
-
-if (argv.length - 2 <= 1) secBig = 0;
-else {
-  argv = argv.slice(2, argv.length);
-  argv.forEach((arg) => {
-    if (parseInt(arg) > biggest) {
-      secBig = biggest;
-      biggest = parseInt(arg);
-    }
-  });
+if (process.argv.length <= 3) {
+  console.log('0');
+} else {
+  const arr = process.argv.slice(2).map(Number);
+  const second = arr.sort(function (a, b) { return b - a; })[1];
+  console.log(second);
 }
-
-console.log(secBig);
--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,7 +3,5 @@ const argList = process.argv;
 if (argList[2] === undefined) {
   console.log('No argument');
 } else {
-  argList.forEach((arg, index) => {
-    if (index > 1) console.log(arg);
-  });
+  console.log(argList[2]);
 }
