3a9114451fe2f42f9eed5491edaf8e64caacf041
Ayowale <haydexwisdom@gmail.com>
2023-09-14 20:15:34 +0100

    Task 2 done

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+2-my_filter_states module
+
+"""
+import MySQLdb
+import sys
+
+
+argv = sys.argv[1:]
+username = argv[0]
+password = argv[1]
+db_name = argv[2]
+state_name = argv[3]
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=db_name
+            )
+    cur = db.cursor()
+    query = "SELECT * FROM states WHERE name = '{}'".format(state_name)
+    cur.execute(query)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
