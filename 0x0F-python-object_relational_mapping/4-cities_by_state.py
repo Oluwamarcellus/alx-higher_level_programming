af36482a3756d3b7df02e7341d63bada35d82183
Ayowale <haydexwisdom@gmail.com>
2023-09-14 20:55:59 +0100

    Task 4 fixed

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -23,9 +23,8 @@ if __name__ == "__main__":
     cur = db.cursor()
     query = """
     SELECT cities.id, cities.name, states.name
-    FROM cities
-    INNER JOIN states
-    ON cities.state_id = states.id
+    FROM cities, states
+    WHERE cities.state_id = states.id
     ORDER BY cities.id ASC
     """
     cur.execute(query)
