5918147c78c62346a304ecddc11c887c32b90041
Ayowale <haydexwisdom@gmail.com>
2023-09-16 10:10:47 +0100

    Task 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+10-model_state_my_get module
+
+"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+
+if __name__ == "__main__":
+    conn = "mysql://{}:{}@localhost:3306/{}"
+    engine = create_engine(conn.format(argv[1], argv[2], argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    Base.metadata.create_all(engine)
+    state = session.query(State).filter_by(name=argv[4]).first()
+    if state:
+        print(state.id)
+    else:
+        print("Not found")
