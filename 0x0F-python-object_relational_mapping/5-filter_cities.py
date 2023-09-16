cbfaaf31956e3c0a69e0db2e4a4b09db45a400c5
Ayowale <haydexwisdom@gmail.com>
2023-09-16 08:57:58 +0100

    Task 5 fixed

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,13 +7,13 @@ import MySQLdb
 import sys
 
 
-argv = sys.argv[1:]
-username = argv[0]
-password = argv[1]
-db_name = argv[2]
-state_name = argv[3]
-
 if __name__ == "__main__":
+    argv = sys.argv[1:]
+    username = argv[0]
+    password = argv[1]
+    db_name = argv[2]
+    state_name = argv[3]
+
     db = MySQLdb.connect(
             host="localhost",
             port=3306,
@@ -26,7 +26,8 @@ if __name__ == "__main__":
     query = """
     SELECT cities.name
     FROM cities, states
-    WHERE cities.state_id = states.id AND states.name = %s
+    WHERE cities.state_id LIKE BINARY states.id
+    AND states.name LIKE BINARY %s
     ORDER BY cities.id ASC
     """
     cur.execute(query, (state_name, ))
