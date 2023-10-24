a6a79e3a40fee8a68b13ab6b68526c505ff09253
Ayowale <haydexwisdom@gmail.com>
2023-10-24 20:53:35 +0100

    Task fixed

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-
 const argv = process.argv.slice(2);
 const request = require('request');
-const url = `https://swapi-api.alx-tools.com/api/films/${argv[0]}`;
-request(url, function (err, res, body) {
+const url = 'https://swapi-api.hbtn.io/api/films/' + argv[0];
+request(url, function (error, response, body) {
   console.log(error || JSON.parse(body).title);
 });
