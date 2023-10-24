e66a86a2d5e7f9f142b8b61f37292daec9c1bbd7
Ayowale <haydexwisdom@gmail.com>
2023-10-24 20:49:48 +0100

    Task 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const argv = process.argv.slice(2);
+const request = require('request');
+const url = `https://swapi-api.alx-tools.com/api/films/${argv[0]}`;
+request(url, (err, res, body) => {
+  console.log(error || JSON.parse(body).title);
+});
